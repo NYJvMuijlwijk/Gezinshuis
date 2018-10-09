43764ddd8a16524c3517e1669437fa425930c7db
Niek <niekvanmuylwijck@gmail.com>
2018-10-09 11:45:17 +0200

    First commit

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php require 'requires/head.php'; ?>
+
+<div class="container content">
+    <div class="row">
+        <div class="col-lg-10">
+
+            <form action="#">
+
+                <label for="gebruikersnaam">Gebruikersnaam</label>
+                <input type="text">
+                <label for="wachtwoord">Wachtwoord</label>
+                <input type="password">
+
+                <input type="submit" value="Inloggen">
+
+            </form>
+
+        </div>
+    </div>
+</div>
+
+<?php require 'requires/foot.php'; ?>
---
+++ requires/nav.php
@@ -0,0 +1,28 @@
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"> <img id="logo" src="./img/small/1.png"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="about.php">Over ons</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="contact.php">contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="login.php">Inloggen</a>
+            </li>
+            <!--<li class="nav-item">-->
+                <!--<a class="nav-link js-scroll-trigger" href="#contact">Contact</a>-->
+            <!--</li>-->
+            </ul>
+        </div>
+    </div>
+</nav>
