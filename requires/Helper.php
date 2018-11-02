35375472579cd7d979cd1aff3c91161de02b2011
Niek <niekvanmuylwijck@gmail.com>
2018-11-02 12:30:15 +0100

    documentation fixes

--- requires/Helper.php
+++ requires/Helper.php
@@ -5,35 +5,32 @@ class Helper
 
     public static function encrypt($plaintext)
     {
-// set variables
+        // set variables
         $key = $_SESSION['key'];
         $method = "aes-256-cbc";
         $iv = $_SESSION['iv'];
-//encrypt data
+        //encrypt data
         $encrypted = openssl_encrypt($plaintext, $method, $key, OPENSSL_RAW_DATA, $iv);
-// base encode  for server readability
+        // base encode  for server readability
         $output = base64_encode($encrypted);
-// return output for database string
+        // return output for database string
         return $output;
     }
 
     public static function decrypt($crypted_text)
     {
-// set variables
+        // set variables
         $key = $_SESSION['key'];
         $method = "aes-256-cbc";
         $iv = $_SESSION['iv'];
-// base decode
+        // base decode
         $decodedtext = base64_decode($crypted_text);
-// decrypt data
+        // decrypt data
         $decrypted = openssl_decrypt($decodedtext, $method, $key, OPENSSL_RAW_DATA, $iv);
-// return $decrypted string
+        // return $decrypted string
         return $decrypted;
     }
 }
-
-
-
 
 /**
  * Created by PhpStorm.
