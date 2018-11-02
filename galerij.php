1a7cdfca5dacf5a22507fe6410539f3a5ebb1e0f
Niek <niekvanmuylwijck@gmail.com>
2018-11-02 12:42:28 +0100

    uploaden naar boven verplaats

--- galerij.php
+++ galerij.php
@@ -6,50 +6,50 @@
     /**
      * Database fetch all images
      */
-        $pdo = $connection->prepare('SELECT file FROM images');
-        $pdo->execute();
-        $value1 = $pdo->fetchall(PDO::FETCH_ASSOC);
-?>
-
-<section>
-    <div class="container content_gallery" style="text-align: center;">
+    $pdo = $connection->prepare('SELECT file FROM images');
+    $pdo->execute();
+    $value1 = $pdo->fetchall(PDO::FETCH_ASSOC);
+    ?>
+    
+    <section>
+        <div class="container content_gallery" style="text-align: center;">
         
-    <h1>galerij</h1>
+        <h1>upload</h1>
+    
+                <div>
+                    <form method="post" enctype="multipart/form-data" action="formaction/upload.php">
+                        <input type="file" name="newimage">
+                        <input type="submit" name="imagesubmit" value="Upload">
+                    </form>
+                </div>
+    
+        </div>
+    </section>
 
-        <div class="resize">
-            <div>
-                <?php
-                    /**
-                     * All images from database *
-                     */
-                    foreach ($value1 as $value2){
-                        foreach ($value2 as $value3) {
-                            ?>
-                            <div class="box">
-                            <?php echo "<img = src=\"data:image/jpg;base64,".base64_encode($value3)."\"/ style=\"width:325px; \">"; ?>
-                            </div>
-                            <?php
+    <section>
+        <div class="container content_gallery" style="text-align: center;">
+            
+        <h1>galerij</h1>
+
+            <div class="resize">
+                <div>
+                    <?php
+                        /**
+                         * All images from database *
+                         */
+                        foreach ($value1 as $value2){
+                            foreach ($value2 as $value3) {
+                                ?>
+                                <div class="box">
+                                <?php echo "<img = src=\"data:image/jpg;base64,".base64_encode($value3)."\"/ style=\"width:325px; \">"; ?>
+                                </div>
+                                <?php
+                            }
                         }
-                    }
-                ?>
+                    ?>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-
-<section>
-    <div class="container content_gallery" style="text-align: center;">
-    
-    <h1>upload</h1>
-
-            <div>
-                <form method="post" enctype="multipart/form-data" action="formaction/upload.php">
-                    <input type="file" name="newimage">
-                    <input type="submit" name="imagesubmit" value="Upload">
-                </form>
-            </div>
-
-    </div>
-</section>
+    </section>
 
 <?php require 'requires/foot.php'; ?>
