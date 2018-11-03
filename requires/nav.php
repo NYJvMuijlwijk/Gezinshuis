b4ec42015bd15c8f7c94ee50ea33cc8916014bca
Niek <niekvanmuylwijck@gmail.com>
2018-11-03 16:51:40 +0100

    profiel dropdown html + css fixes

--- requires/nav.php
+++ requires/nav.php
@@ -3,44 +3,46 @@
     
     <a class="navbar-brand" href="index.php"><img id="logo" src="./img/small/1.png"></a>
 
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTopMenu" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTopMenu" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarTopMenu">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="index.php">home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="about.php">over ons</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link js-scroll-trigger" href="contact.php">contact</a>
-            </li>
-            <li class="nav-item">
+        <div class="collapse navbar-collapse" id="navbarTopMenu">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="index.php">home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="about.php">over ons</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="contact.php">contact</a>
+                </li>
+                <li class="nav-item">
 
-                <?php if(!$_SESSION['loginstatus']) { ?>
-                    <a class="nav-link js-scroll-trigger" href="login.php">Inloggen</a>
-                <?php } else { ?>
-                    
-                    <div class="dropdown show">
-                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <?= $_SESSION['user']->fname; ?>
-                            <img src="img/profile.png" alt="profile picture">
-                        </a>
+                    <?php if(!$_SESSION['loginstatus']) { ?>
+                        <a class="nav-link js-scroll-trigger" href="login.php">Inloggen</a>
+                    <?php } else { ?>
 
-                        <div class="dropdown-menu" aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="user.php">Gebruikersomgeving</a>
-                            <a class="dropdown-item" href="logout.php">Uitloggen</a>
-                        </div>
-                    </div>
+                </li>
 
-                <?php } ?>
+                
 
-            </li>
-        </ul>
-    </div>
+            </ul>
+        </div>
 
+        <div class="dropdown">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div class="hideable"><?= $_SESSION['user']->fname; ?></div>
+                <img src="img/profile.png" alt="profile picture">
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="user.php">Gebruikersomgeving</a>
+                <a class="dropdown-item" href="logout.php">Uitloggen</a>
+            </div>
+        </div>
+
+        <?php } ?>
 
 </nav>
